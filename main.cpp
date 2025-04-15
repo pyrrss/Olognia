a6ccf0cd95c94037618df6206a1b37804bffa7bc
pyrr <oliversisaias04@gmail.com>
2025-04-15 17:18:15 -0400

    [FIX] Arreglado caso en que CANO muera antes de dividirse [FIX] Arreglado caso en que hijos de CANO tengan vida 0 [FEATURE] Agregada carpeta de archivos con casos de ejemplo para testear

--- main.cpp
+++ main.cpp
@@ -23,18 +23,20 @@ struct Enemy
 
 int main()
 {
+    bool debug = true;
+
     Mechon mechon;
 
-    std::cout << "Vida del héroe mechón: ";
+    // std::cout << "Vida del héroe mechón: ";
     std::cin >> mechon.health; 
 
     int num_enemies;
-    std::cout << "Número total de esbirros: ";
+    // std::cout << "Número total de esbirros: ";
     std::cin >> num_enemies;
   
     std::deque<Enemy> *enemies = new std::deque<Enemy>();
 
-    std::cout << "Vida de los esbirros (separada por espacios): ";
+    // std::cout << "Vida de los esbirros (separada por espacios): ";
     for(size_t current_enemy_index = 0; current_enemy_index < num_enemies; current_enemy_index++)
     {
         Enemy enemy;
@@ -42,60 +44,70 @@ int main()
         enemies->push_back(enemy);
     }
 
-    std::cout << "Daño de los esbirros (separada por espacios): ";
+    // std::cout << "Daño de los esbirros (separada por espacios): ";
     for(size_t current_enemy_index = 0; current_enemy_index < num_enemies; current_enemy_index++)
     {
         std::cin >> enemies->at(current_enemy_index).damage;
     }
 
-    std::cout << "Esbirros especiales (separada por espacios): ";
+    // std::cout << "Esbirros especiales (separada por espacios): ";
     for(size_t current_enemy_index = 0; current_enemy_index < num_enemies; current_enemy_index++)
     {
         std::cin >> enemies->at(current_enemy_index).is_special;
     }
 
     // -> estado de la deque
-    for(Enemy& enemy : *enemies)
+    if(debug)
     {
-        std::cout << "Vida: " << enemy.health << " Daño: " << enemy.damage << " Especial: " << enemy.is_special << std::endl;
+        for(Enemy& enemy : *enemies)
+        {
+            // std::cout << "Vida: " << enemy.health << " Daño: " << enemy.damage << " Especial: " << enemy.is_special << std::endl;
+        }
     }
+
 
     int total_damage_to_enemies = 0;
     int enemies_defeated = 0;
     int current_enemy_index = 0;
 
-    // -> pelea
+    // -> "game-loop"
     // error que tomó más tiempo fue iterar sobre la deque mientras se modificaba (al dividir enemigo especial)
     // solución fue no iterar sobre deque, sino tomar siempre al enemigo de al frente y listo
     while(!enemies->empty())
     {
         Enemy& enemy = enemies->front();
-        std::cout << "Duelo " << current_enemy_index + 1 << std::endl;
+        if(debug) { std::cout << "Duelo " << current_enemy_index + 1 << std::endl; }
  
         int current_round = 0;
         while(enemy.health > 0 && mechon.health > 0)
         {
             current_round++;
-            std::cout << "Ronda " << current_round << std::endl;
+            if(debug){ std::cout << "Ronda " << current_round << std::endl; }
 
-            // -> ataque mechon a enemigo
-            std::cout << "Mechon ataca con " << mechon.damage << " a enemigo, "; 
-            int damage_to_enemy = std::min(mechon.damage, enemy.health);
-            enemy.health -= damage_to_enemy;
-            total_damage_to_enemies += damage_to_enemy;
-            std::cout << "causa " << damage_to_enemy << " de daño ";
 
             // -> ataque enemigo a mechon
             int damage_to_mechon = std::min(enemy.damage, mechon.health);
             mechon.health -= damage_to_mechon;
-            std::cout << "y recibe " << damage_to_mechon << " de daño" << std::endl;
+
+            // -> ataque mechon a enemigo
+            int damage_to_enemy = std::min(mechon.damage, enemy.health);
+            enemy.health -= damage_to_enemy;
+            total_damage_to_enemies += damage_to_enemy;
+            
+            if(debug)
+            {
+                std::cout << "Mechon ataca con " << mechon.damage << 
+                    " y causa " << damage_to_enemy << 
+                    " a enemigo, " << " y recibe " <<
+                    damage_to_mechon << " de daño" << std::endl;
+            }
 
             // lo siguiente es manejar caso enemigo especial
 
             // si el enemigo es especial, luego de recibir 2 ataques, se dividirá en 2 enemigos con 1 menos de vida y ataque
             if(current_round == 2 && enemy.is_special && enemy.health > 0)
             {
-                std::cout << "Enemigo especial se divide en 2" << std::endl;
+                if(debug) { std::cout << "Enemigo especial se divide en 2" << std::endl; }
                 Enemy new_enemy_a;
 
                 new_enemy_a.health = enemy.health - 1;
@@ -108,26 +120,30 @@ int main()
                 enemies->push_front(new_enemy_a);
                 enemies->push_front(new_enemy_b);
                 
-                std::cout << "Nuevo estado de la deque: " << std::endl;
-                for(Enemy& enemy : *enemies)
+                if(debug)
                 {
-                    std::cout << "Vida: " << enemy.health << " Daño: " << enemy.damage << " Especial: " << enemy.is_special << std::endl;
+                    std::cout << "Nuevo estado de la deque: " << std::endl;
+                    for(Enemy& enemy : *enemies)
+                    {
+                        std::cout << "Vida: " << enemy.health << " Daño: " << enemy.damage << " Especial: " << enemy.is_special << std::endl;
+                    }
                 }
                 
                 break;
             }
 
 
-            std::cout << "(Daño acumulado : " << total_damage_to_enemies << ")" << std::endl;
+            if(debug) { std::cout << "(Daño acumulado : " << total_damage_to_enemies << ")" << std::endl; }
         }
 
         if(enemy.health <= 0)
         {
-            std::cout << "Enemigo derrotado" << std::endl;
+            if(debug) { std::cout << "Enemigo derrotado" << std::endl; }
             enemies_defeated++;
             enemies->pop_front();
             current_enemy_index++;    
 
+            // TODO: Hacer que aumente cada 5
             if(enemies_defeated == 6)
             {
                 mechon.damage++;
@@ -137,7 +153,7 @@ int main()
 
         if(mechon.health <= 0)
         {
-            std::cout << "Mechón derrotado" << std::endl;
+            if(debug) { std::cout << "Mechón derrotado" << std::endl; }
             break;
         }
 
